3bf6a12c79ed1d2505c69b3c6823b35e7582d048
keiondesu96 <keiondesu96@gmail.com>
2020-04-19 01:50:26 +0300

    player animations

--- Ludum46/Code/Reusable/Player/PlayerDrawer.cs
+++ Ludum46/Code/Reusable/Player/PlayerDrawer.cs
@@ -19,27 +19,43 @@ namespace Ludum46.Code.Reusable
 
         public static Vector2 unscaledCameraDrawOffset => (PlayerDataManager.unscaledPixelPosition) - cameraRelative;
 
-        public static Rectangle Rect => new Rectangle(PlayerDataManager.unscaledPixelPosition.ToPoint(), playerSize.ToPoint());
+        public static Rectangle Rect => new Rectangle(PlayerDataManager.unscaledPixelPosition.ToPoint(), playerFrame.ToPoint());
 
-        private static Vector2 playerSize;
+        private static Vector2 playerFrame;
 
         private Ludum46 refGame;
+
         private Texture2D playerStill;
+        private Animation sheetLeft;
+        private Animation sheetRight;
+        private Animation sheetUp;
+        private Animation sheetDown;
+        private Animation sheetUpLeft;
+        private Animation sheetUpRight;
+        private Animation sheetDownLeft;
+        private Animation sheetDownRight;
 
         public PlayerDrawer(Ludum46 game)
         {
             this.refGame = game;
 
-            //Debug
-            playerStill = null;
-            playerSize = new Vector2(12, 12);
+            playerFrame = new Vector2(24, 24);
+            cameraRelative = new Vector2(Ludum46.UnscaledWidth - playerFrame.X, Ludum46.UnscaledHeight - playerFrame.Y) / 2;
 
-            cameraRelative = new Vector2(Ludum46.UnscaledWidth - playerSize.X, Ludum46.UnscaledHeight - playerSize.Y) / 2;
+            int frameMs = 150;
+            sheetLeft = new Animation(game, "res/entity/player/playerLeft", (int)playerFrame.X, frameMs);
+            sheetRight = new Animation(game, "res/entity/player/playerRight", (int)playerFrame.X, frameMs);
+            sheetUp = new Animation(game, "res/entity/player/playerUp", (int)playerFrame.X, frameMs);
+            sheetDown = new Animation(game, "res/entity/player/playerDown", (int)playerFrame.X, frameMs);
+            sheetUpLeft = new Animation(game, "res/entity/player/playerUpLeft", (int)playerFrame.X, frameMs);
+            sheetUpRight = new Animation(game, "res/entity/player/playerUpRight", (int)playerFrame.X, frameMs);
+            sheetDownLeft = new Animation(game, "res/entity/player/playerDownLeft", (int)playerFrame.X, frameMs);
+            sheetDownRight = new Animation(game, "res/entity/player/playerDownRight", (int)playerFrame.X, frameMs);
         }
 
-        internal void Draw(SpriteBatch batch)
+        public void Draw(SpriteBatch batch)
         {
-            refGame.screenPool.drawShape.Draw(batch, Color.MediumOrchid, PlayerDataManager.unscaledPixelPosition - unscaledCameraDrawOffset, new Vector2(12, 12), Ludum46.Scale);
+            refGame.screenPool.drawShape.Draw(batch, Color.MediumOrchid, PlayerDataManager.unscaledPixelPosition - unscaledCameraDrawOffset, playerFrame, Ludum46.Scale);
         }
     }
 }
