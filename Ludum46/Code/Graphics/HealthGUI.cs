c28724dfa0e0edfb3051433555f81e817466f244
keiondesu96 <keiondesu96@gmail.com>
2020-04-21 02:47:39 +0300

    hp on center

--- Ludum46/Code/Graphics/HealthGUI.cs
+++ Ludum46/Code/Graphics/HealthGUI.cs
@@ -14,18 +14,22 @@ namespace Ludum46.Code.Graphics
         static private Texture2D gui;
         static private Texture2D orb;
 
+        static private int offsetX;
+
         static public void InitAsset(Ludum46 game)
         {
             gui = game.Content.Load<Texture2D>("res/gui/hp_bar");
             orb = game.Content.Load<Texture2D>("res/gui/hp");
+
+            offsetX = (int)((Ludum46.UnscaledWidth - gui.Width) / 2);
         }
 
         static public void Draw(SpriteBatch batch)
         {
-            DrawTexture(batch, gui, Vector2.Zero, Ludum46.Scale);
+            DrawTexture(batch, gui, new Vector2(offsetX, 0)* Ludum46.Scale, Ludum46.Scale);
             for (int i = 0; i < PlayerDataManager.hp; i++)
             {
-                var pos = new Vector2(6 + 16*i, 1) * Ludum46.Scale;
+                var pos = new Vector2(6 + 16*i + offsetX, 1) * Ludum46.Scale;
                 DrawTexture(batch, orb, pos, Ludum46.Scale);
             }
         }
