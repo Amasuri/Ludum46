034573e4e24fb7a6ef2b535d123d960a5bfae2c0
keiondesu96 <keiondesu96@gmail.com>
2020-04-20 21:15:36 +0300

    stone

--- Ludum46/Code/Level/EntityStone.cs
+++ Ludum46/Code/Level/EntityStone.cs
@@ -17,7 +17,11 @@ namespace Ludum46.Code.Level
         public EntityStone(Ludum46 game, string imgPath, Vector2 coord, bool initRectAsImage = true)
             : base(game, imgPath, coord, initRectAsImage, Int32.MaxValue)
         {
-            this.relativeRect = new Rectangle(0, 0, 12, 12);
+            this.relativeRect = new Rectangle(0, 13, 18, 12);
+            this.rectList[0] = new Rectangle(this.unsCoord.ToPoint() + this.relativeRect.Location, this.relativeRect.Size);
+
+            this.stoneAnime = new Animation(game, "res/entity/heart", 18, Ludum46.Scale, 150 );
+            this.stoneAnime.EnableDrawing();
         }
 
         public void TryPush(Vector2 move, Room actionRoom)
@@ -34,9 +38,13 @@ namespace Ludum46.Code.Level
             this.rectList[0] = new Rectangle(this.unsCoord.ToPoint() + this.relativeRect.Location, this.relativeRect.Size);
         }
 
-        public override void Draw(SpriteBatch spriteBatch, Vector2 unsCamera)
+        public override void Draw(SpriteBatch batch, Vector2 unsCamera)
         {
-            base.Draw(spriteBatch, unsCamera);
+            var pos = this.unsCoord - unsCamera;
+
+            stoneAnime.Draw(batch, pos * Ludum46.Scale, SpriteEffects.None, Ludum46.DeltaDraw);
+
+            base.Draw(batch, unsCamera);
         }
     }
 }
