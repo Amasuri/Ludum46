926edaf20c86a6d4ed3ee74009886432b97efd13
keiondesu96 <keiondesu96@gmail.com>
2020-04-20 21:24:26 +0300

    rekt chek

--- Ludum46/Code/Level/EntityStone.cs
+++ Ludum46/Code/Level/EntityStone.cs
@@ -17,7 +17,7 @@ namespace Ludum46.Code.Level
         public EntityStone(Ludum46 game, string imgPath, Vector2 coord, bool initRectAsImage = true)
             : base(game, imgPath, coord, initRectAsImage, Int32.MaxValue)
         {
-            this.relativeRect = new Rectangle(0, 13, 18, 12);
+            this.relativeRect = new Rectangle(0, 0, 18, 25);
             this.rectList[0] = new Rectangle(this.unsCoord.ToPoint() + this.relativeRect.Location, this.relativeRect.Size);
 
             this.stoneAnime = new Animation(game, "res/entity/heart", 18, Ludum46.Scale, 150 );
@@ -44,7 +44,8 @@ namespace Ludum46.Code.Level
 
             stoneAnime.Draw(batch, pos * Ludum46.Scale, SpriteEffects.None, Ludum46.DeltaDraw);
 
-            base.Draw(batch, unsCamera);
+            //Debug
+            //base.Draw(batch, unsCamera);
         }
     }
 }
