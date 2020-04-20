d507a30d404ce910fe9e0f7c76a5f5c5c58812a4
keiondesu96 <keiondesu96@gmail.com>
2020-04-21 00:43:20 +0300

    credits + font

---
+++ Ludum46/Code/Graphics/EndScreen.cs
@@ -0,0 +1,29 @@
+﻿using Ludum46.Code.Reusable;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ludum46.Code.Graphics
+{
+    public class EndScreen : ADrawingClass
+    {
+        public EndScreen(Ludum46 game)
+        {
+        }
+
+        public override void Draw(Game game, SpriteBatch spriteBatch)
+        {
+            ;
+        }
+
+        public override void Update(Game game, MouseState mouse, MouseState oldMouse, KeyboardState keys, KeyboardState oldKeys)
+        {
+            ;
+        }
+    }
+}
