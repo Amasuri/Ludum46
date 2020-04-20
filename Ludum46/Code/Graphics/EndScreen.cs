1157ff045b7af65f34e2f9656525a288062a9601
keiondesu96 <keiondesu96@gmail.com>
2020-04-21 01:11:23 +0300

    room + endsc

--- Ludum46/Code/Graphics/EndScreen.cs
+++ Ludum46/Code/Graphics/EndScreen.cs
@@ -15,6 +15,8 @@ namespace Ludum46.Code.Graphics
         private Pixel pixel;
         private int pixAlpha = 0;
 
+        private const string EndText = "You have fallen in battle,\n and therefore, your tree has died.";
+
         public EndScreen(Ludum46 game)
         {
             pixel = new Pixel(game.GraphicsDevice);
@@ -23,6 +25,11 @@ namespace Ludum46.Code.Graphics
         public override void Draw(Game game, SpriteBatch spriteBatch)
         {
             pixel.Draw(spriteBatch, new Color(0, 0, 0, pixAlpha), Vector2.Zero, new Vector2(Ludum46.ScaledWidth, Ludum46.ScaledHeight));
+
+            var size = ScreenPool.fontBig.MeasureString(EndText);
+            var pos = (new Vector2(Ludum46.ScaledWidth, Ludum46.ScaledHeight) - size) / 2;
+
+            spriteBatch.DrawString(ScreenPool.fontBig, EndText, pos, new Color(200, 200, 200, pixAlpha));
         }
 
         public override void Update(Game game, MouseState mouse, MouseState oldMouse, KeyboardState keys, KeyboardState oldKeys)
